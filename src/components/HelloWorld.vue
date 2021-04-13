f1b5724d1f2010f8dce76c2ca374e84e9f7b1b2f
Goomba41 <anton.borodawkin@yandex.ru>
2021-04-13 20:48:15 +0300

    VueJS 2 Project initiation and vue-leaflet plugin test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          target="_blank"
+          rel="noopener"
+          >babel</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          target="_blank"
+          rel="noopener"
+          >eslint</a
+        >
+      </li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+          >Forum</a
+        >
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+          >Community Chat</a
+        >
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+          >Twitter</a
+        >
+      </li>
+      <li>
+        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+      </li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li>
+        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+          >vue-router</a
+        >
+      </li>
+      <li>
+        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-devtools#vue-devtools"
+          target="_blank"
+          rel="noopener"
+          >vue-devtools</a
+        >
+      </li>
+      <li>
+        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+          >vue-loader</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+          rel="noopener"
+          >awesome-vue</a
+        >
+      </li>
+    </ul>
+
+    <div style="height: 500px; width: 100%">
+      <l-map
+        v-if="showMap"
+        :zoom="zoom"
+        :center="center"
+        :options="mapOptions"
+        style="height: 80%"
+      >
+        <l-tile-layer :url="url" :attribution="attribution" />
+        <l-marker-cluster>
+          <l-marker :lat-lng="withPopup">
+            <l-popup>
+              <div>
+                I am a popup
+                <p v-show="showParagraph">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Quisque sed pretium nisl, ut sagittis sapien. Sed vel
+                  sollicitudin nisi. Donec finibus semper metus id malesuada.
+                </p>
+              </div>
+            </l-popup>
+          </l-marker>
+          <l-marker :lat-lng="withPopup2">
+            <l-popup>
+              <div>
+                I am a popup
+                <p v-show="showParagraph">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Quisque sed pretium nisl, ut sagittis sapien. Sed vel
+                  sollicitudin nisi. Donec finibus semper metus id malesuada.
+                </p>
+              </div>
+            </l-popup>
+          </l-marker>
+        </l-marker-cluster>
+      </l-map>
+    </div>
+  </div>
+</template>
+
+<script>
+import { latLng } from "leaflet";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      zoom: 13,
+      center: latLng(58.603548647788486, 49.66802292014676),
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: latLng(58.614386116848614, 49.6840178479317),
+      withPopup2: latLng(58.61417899981327, 49.68506927386554),
+      currentZoom: 11,
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5,
+      },
+      showMap: true,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
